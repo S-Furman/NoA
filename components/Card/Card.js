2d1c9d320691c7c81d2a6c4f313a4a3a5f483f29
S-Furman <szymon.fur@o2.pl>
2021-05-27 19:36:41 +0200

    card component added

---
+++ components/Card/Card.js
@@ -0,0 +1,17 @@
+import classes from "./Card.module.css";
+import Image from "next/image";
+
+const Card = (props) => {
+  return (
+    <article className={classes.article}>
+      <div className={classes.leftSideContainer}>
+        <header className={classes.cardHeader}>{props.title}</header>
+        <p>{props.content}</p>
+      </div>
+      <div className={classes.rightSideContainer}>
+        <Image src={props.imgPath} layout="fill" objectFit="cover" />
+      </div>
+    </article>
+  );
+};
+export default Card;
